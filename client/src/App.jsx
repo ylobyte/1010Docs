74bbc519105a05a05c0d903b3bb21457569b6f70
Kwaku Boateng <kwakuboateng.dev@gmail.com>
2023-11-07 03:19:54 +0000

    created routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar.component";
 
 const App = () => {
     return (
-        <Navbar />
+        <Routes>
+            <Route path='/' element={<Navbar />}>
+                <Route path='signin' element={<h1>Sign in Page</h1>}/>
+                <Route path='signup' element={<h1>Sign up Page</h1>}/>
+            </Route>
+        </Routes>
     )
 }
 
